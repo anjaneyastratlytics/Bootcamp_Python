7878f4508390b3b1d9eaefc37a913a6138da16ea
Anjaneya <anjaneya.pattnaik@stratlytics.com>
2026-07-14 15:13:24 +0530

    Unit 2 Lesson 2 Completed

--- 04_Deliverables/02_Unit2/02_U2L2.py
+++ 04_Deliverables/02_Unit2/02_U2L2.py
@@ -1,4 +1,10 @@
 import csv
+import logging
+
+logging.basicConfig(
+    level = logging.INFO,
+    format = "%(asctime)s | %(levelname)s | %(message)s"
+)
 
 # root_path = "C:/Users/KIIT/Desktop/Stratlytics/02_Bootcamp/04_Python/"
 root_path = "/home/name04/Desktop/Anjaneya/02_Bootcamp/04_Python/Bootcamp_Python/"
@@ -22,18 +28,17 @@ def main(file_path,limit=5):
     '''Orchestrates file read and validation upto a specified limit'''
     valid_row_count = 0
     row_count = 0
-    
+    logging.info(f"Ingestion started | {file_path.split('/')[-1]}")
     for row in read_inventory(file_path):
-        row_count += 1
         if valid_row_count >= limit:
             break
+        row_count += 1
         if validate_headers(row):
             valid_row_count += 1
-            print(f"[INFO] Valid Row {valid_row_count}:\n{row}")
+            logging.info(f"Validated | inventory_id = {row.get('inventory_id')}")
         else:
-            print(f"[INFO] Header mismatch for file row number {row_count}")
-            raise ValueError("Header mismatch")
-    print(f"[INFO] Found and printed {valid_row_count} rows.")
+            logging.error(f" Header mismatch | inventory_id = {row.get('inventory_id')}")
+    logging.info(f"Validation Complete | Valid rows = {valid_row_count}, Invalid rows = {row_count-valid_row_count}")
 
 # main(inv_file_path)
 main(test_inv_file_path)
