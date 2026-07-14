f6da6de12547f2e75424b4877d8dc0e25046419d
Anjaneya <anjaneya.pattnaik@stratlytics.com>
2026-07-14 14:30:37 +0530

    Unit 2 Lesson 1 completed

---
+++ 04_Deliverables/02_Unit2/01_U2L1.py
@@ -0,0 +1,38 @@
+import csv
+
+# root_path = "C:/Users/KIIT/Desktop/Stratlytics/02_Bootcamp/04_Python/"
+root_path = "/home/name04/Desktop/Anjaneya/02_Bootcamp/04_Python/Bootcamp_Python/"
+inv_file_path = root_path + "01_Data/dirty/inventory.csv"
+test_inv_file_path = root_path + "01_Data/dirty/test_inventory.csv"
+
+def read_inventory(file_path):
+    '''Generator-based file reader'''
+    with open(file_path, newline="") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            yield row
+
+def validate_headers(row):
+    '''Validates exact header matching for each row'''
+    expected_headers = {"inventory_id","snapshot_date","dealer_id","product_id","on_hand_qty","on_order_qty","reorder_point","reorder_qty","last_restock_date","last_sale_date"}
+    actual_headers = set(row.keys())
+    return actual_headers == expected_headers
+
+def main(file_path,limit=5):
+    '''Orchestrates file read and validation upto a specified limit'''
+    valid_row_count = 0
+    row_count = 0
+    for row in read_inventory(file_path):
+        row_count += 1
+        if valid_row_count >= limit:
+            break
+        if validate_headers(row):
+            valid_row_count += 1
+            print(f"[INFO] Valid Row {valid_row_count}:\n{row}")
+        else:
+            print(f"[INFO] Header mismatch for file row number {row_count}")
+            raise ValueError("Header mismatch")
+    print(f"[INFO] Found and printed {valid_row_count} rows.")
+
+# main(inv_file_path)
+main(test_inv_file_path)
